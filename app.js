e319f651a6bff56ed81899b9bcad2e4fc84735e2
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-29 03:06:05 +0530

    Updated App.js for moveNames route using AutoPilot

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8009;
 
 require('dotenv').config();
 
+const moveNamesRoute = require('./routes/moveNames');
+app.use('/moveNames', moveNamesRoute);
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
